d8d05912d3da7a9d9f3abbe5461a17179f713974
Eduardo Marrero González <edumarreroglezz@gmail.com>
2024-06-11 18:16:18 +0200

    TOPOLOGICAL_SORT_DFS_ITERATIVE SOLVED

---
+++ EXERCISES/DFS/TOPOLOGICAL_SORT_DFS_RECURSIVE/solve.py
@@ -0,0 +1,51 @@
+import networkx as nx
+
+
+def dfs_topological_sort(graph):
+    """
+    Compute one topological sort of the given graph.
+    """
+
+    # La solucion que retorna esta función es un diccionario de Python.
+    #   * La clave del diccionario es el número del nodo
+    #   * El valor es el orden topologico asignado a ese nodo
+    #
+    # Por ejemplo, si tenemos el siguiente grafo dirigido con 3 vertices:
+    #                    3 ---> 2 ---> 1
+    # ... el orden topologico es:
+    #                El vértice 3 va en la primera posición
+    #                El vértice 2 en la segunda posición
+    #                El vértice 1 en la tercera posición
+    # Con lo que debemos devolver un diccionario con este contenido:
+    #     {1: 3, 2: 2, 3: 1}
+
+    N = graph.number_of_nodes()
+
+    visibleNodes = set()  # En este ejercicio utilizamos un set
+    # para recordar los nodos visibles
+    order = {}
+
+    # solve it here! ------------------------------------------------
+
+    def dfs(u):
+        nonlocal N
+
+        visibleNodes.add(u)
+
+        for neighbor in graph.neighbors(u):
+            if neighbor not in visibleNodes:
+                dfs(neighbor)
+
+        order[u] = N
+        N -= 1
+        return
+
+    # 2. Añade código también aqui
+    # ...
+
+    for node in graph.nodes():
+        if node not in visibleNodes:
+            dfs(node)
+
+
+    return order
